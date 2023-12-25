82c0caea57a43b4aa14357776c1ca13fd2053987
Hasankhankor <55666504+Hasankhankor@users.noreply.github.com>
2023-12-25 22:10:48 +0500

    hasan1

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,14 +4,12 @@ import Navigation from "./Navigation";
 
 export default function Header() {
     return (
+<div className="sticky inset-x-0 top-0 z-10 bg-opacity-60 bg-zinc-100 backdrop-blur-lg">
+    <div className="container flex items-center justify-between py-8">
+        <Image src={logo} alt="logo" className="w-30 h-14 object-cover" />
+        <Navigation />
+    </div>
+</div>
 
-        <div className="sticky inset-x-0 top-0 z-10 bg-opacity-60 bg-zinc-100 backdrop-blur-lg">
-            <div className="container flex items-center justify-between py-8 ">
-
-                <Image src={logo} alt="logo" className="w-40 h-6" />
-
-                <Navigation />
-            </div>
-        </div>
     );
 }
